2b286e5afa1f25a35321e2b55c86e3cf8f08ddf0
n1pingo <joaogn.neto@gmail.com>
2020-07-31 21:41:05 -0300

    Projeto DDD - v1

---
+++ ProjetoApplication/Contracts/IUsuarioApplicationService.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoApplication.Models;
+using Projeto.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Application.Contracts {
+    public interface IUsuarioApplicationService {
+        void Insert(UsuarioCadastroModel model);
+
+        Usuario GetByLogin(string login);
+
+        Usuario GetByLoginAndSenha(UsuarioAutenticacaoModel model);
+    }
+}
